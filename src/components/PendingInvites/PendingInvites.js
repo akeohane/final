1eef8c1022ea6a209d405da007f7ccd6fa471e76
akeohane <34900796+akeohane@users.noreply.github.com>
2018-11-19 19:23:15 -0700

    Update PendingInvites.js

--- src/components/PendingInvites/PendingInvites.js
+++ src/components/PendingInvites/PendingInvites.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./PendingInvites.css";
 
 const PendingInvite = props => (
-<div className="cunt">    
+<div className="placeholder">    
 <div className="card w-75">
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
